0048f20fd16179bd3356f6d7df00ad170581a2df
Arek Ohanian <Ohaniana@go.stockton.edu>
2023-02-02 18:09:47 -0500

    some changes

--- week1/test.java
+++ week1/test.java
@@ -2,5 +2,10 @@ package week1;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String color = "green";
+        // this is a variable for my favorite color obviously
+        System.out.println("My favorite color is" + " " + color);
+        // this is printing out that my favoirte color is green to the terminal
+        
     }
 }
