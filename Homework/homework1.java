d6f2894c6366548ec3bbc5979b7da4fd146dece2
Arek Ohanian <Ohaniana@go.stockton.edu>
2023-02-09 19:19:39 -0500

    some commits

--- Homework/homework1.java
+++ Homework/homework1.java
@@ -3,5 +3,10 @@ package Homework;
 public class homework1 {
     public static void main(String[]args){
         System.out.println("Hello World!");
+        System.out.println("My favorite game is Shin Megami Tensei IV for the Nintendo 3DS.");
+        String name = "Arek";
+        System.out.println("My name is"+ " " + name);
+        System.out.println ("I want to learn how to code because I want to be a game developer.");
+        System.out.println("Java is not a good languiage for game development because it is not as fast as c# or c++."); 
     }
 }
